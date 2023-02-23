d12ba19bdd14456e9661bc0560908669e1494c0f
Lionlancer <lionlancer@gmail.com>
2023-02-23 15:25:18 +0800

    small update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -15,14 +15,24 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <button type="button" className="btn" onClick={navigateToAuth}>
-        Login
-      </button>
-      <button type="button" className="btn" onClick={navigateToRegister}>
-        Register
-      </button>
+    <div className="w-full h-full flex center-items">
+      <div className="m-auto p-10 text-center">
+        <h1 className="font-bold text-4xl">Shopping Cart</h1>
+        <button
+          type="button"
+          className="btn w-full mt-10"
+          onClick={navigateToAuth}
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          className="btn w-full my-5"
+          onClick={navigateToRegister}
+        >
+          Register
+        </button>
+      </div>
     </div>
   );
 }
